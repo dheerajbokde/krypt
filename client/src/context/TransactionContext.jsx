ecde6871db4cf1cf06c4627e15ba33ebe5b1e63f
dheera bokde <dheerajbokde21@gmail.com>
2022-06-27 09:58:57 +0530

    services, transactions(dummy+real data), footer, create hook-useFetch

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -20,13 +20,40 @@ export const TransactionProvider = ({ children }) => {
     const [formData, setFormData] = useState({ addressTo: '', amount: '', keyword: '', message: '' });
     const [isLoading, setIsLoading] = useState(false);
     const [transactionCount, setTransactionCount] = useState(localStorage.getItem('transactionCount'));
+    const [transactions, setTransactions] = useState([]);
 
     const handleChange = (e, name) => {
         setFormData((prevState) => ({ ...prevState, [name]: e.target.value }));
     }
 
+    const getAllTransactions = async() =>{
+        try {
+            if (!ethereum) return alert("Please install Metamask");
+            
+            const transactionContract = getEthereumContract();
+            const availableTransactions = await transactionContract.getAllTransactions();
+            // console.log("availableTransactions", availableTransactions);
+            
+            const structuredTransactions = await availableTransactions.map((transaction) => ({
+                addressTo: transaction.receiver,
+                addressFrom: transaction.sender,
+                timestamp: new Date(transaction.timestamp.toNumber() * 1000).toLocaleString(),
+                message: transaction.message,
+                keyword: transaction.keyword,
+                amount: parseInt(transaction.amount._hex) / (10 ** 18)
+            }))
+            
+            console.log("structuredTransactions", structuredTransactions);
+            setTransactions(structuredTransactions);
+        } catch (error) {
+            
+        }
+    }
+
+
     useEffect(() => {
         checkIfWalletIsConnected();
+        checkIfTransactionsExists();
     }, []);
 
     const checkIfWalletIsConnected = async () => {
@@ -38,6 +65,7 @@ export const TransactionProvider = ({ children }) => {
             if (accounts.length) {
                 setCurrentAccount(accounts[0]);
                 //getAllTransactions();
+                getAllTransactions();
             } else {
                 console.log('No accounts found');
             }
@@ -79,7 +107,7 @@ export const TransactionProvider = ({ children }) => {
             });
 
             // send ethereum and add transaction on blockchain
-            const transactionHash = await transactionContract.transfer(addressTo, parseAmount, message, keyword);//add to blockchain
+            const transactionHash = await transactionContract.addToBlockchain(addressTo, parseAmount, message, keyword);//add to blockchain
             setIsLoading(true);
             console.log(`Loading - ${transactionHash.hash}`)
             await transactionHash.wait();
@@ -89,6 +117,19 @@ export const TransactionProvider = ({ children }) => {
             // update transaction count
             const transactionCount = await transactionContract.getTransactionCount();
             setTransactionCount(transactionCount.toNumber());
+            
+            window.reload();
+        } catch (error) {
+            console.log(error);
+            throw new Error("No ethereum object");
+        }
+    }
+
+    const checkIfTransactionsExists = async() => {
+        try {
+            const transactionContract = getEthereumContract();
+            const transactionCount = await transactionContract.getTransactionCount();
+            window.localStorage.setItem("transactionCount", transactionCount);
         } catch (error) {
             console.log(error);
             throw new Error("No ethereum object");
@@ -96,7 +137,7 @@ export const TransactionProvider = ({ children }) => {
     }
 
     return (
-        <TransactionContext.Provider value={{ connectWallet, currentAccount, formData, setFormData, handleChange, sendTransaction }}>
+        <TransactionContext.Provider value={{ connectWallet, currentAccount, formData, setFormData, handleChange, sendTransaction, transactions, isLoading }}>
             {children}
         </TransactionContext.Provider>
     );
--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,6 @@
 import abi from './Transactions.json';
 
 export const contractABI = abi.abi;
+//export const contractAddress = '0x7bB0c573F7ab99301593A8dFC5F71ACc5448Dc53';
+//ropsten
 export const contractAddress = '0xe0d0c4C67B291a160265a4c002a77d503aD01563';
